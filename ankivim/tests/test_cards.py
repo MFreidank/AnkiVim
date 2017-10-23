c7f9bebf52e6d23d6f0fa7161dfcc7c963633777
MFreidank <freidankm@yahoo.de>
2017-10-23 13:43:45 +0200

    interim

--- ankivim/tests/test_cards.py
+++ ankivim/tests/test_cards.py
@@ -38,7 +38,8 @@ def test_editor_command_empty_args():
             editor=editor, editor_args=editor_args, filename=filename
         )
 
-        assert cmd == (editor, *editor_args, filename)
+        expected_cmd = (editor, *editor_args, filename)
+        assert cmd == expected_cmd
 
 
 def test_emacs_args():
