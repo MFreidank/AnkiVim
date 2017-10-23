4740753cfad3105280ccd5f165ff4164a676e370
MFreidank <freidankm@yahoo.de>
2017-10-23 18:59:59 +0200

    adding files for PyPI

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ from setuptools import setup, find_packages
 with open("README.rst", "r") as f:
     long_description = f.read()
 
+VERSION = "1.0"
+
 
 setup(name="VimForAnki",
-      version="1.0",
+      version=VERSION,
       url="https://github.com/MFreidank/VimForAnki",
+      download_url="https://github.com/MFreidank/VimForAnki/tarball/{}".format(VERSION),
       description="Use vim to rapidly write textfiles immediately importable into anki(1).",
       long_description=long_description,
       author="Moritz Freidank",
