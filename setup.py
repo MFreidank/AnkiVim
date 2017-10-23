e92f203bf51d5e597f07676c075f42726a852db0
MFreidank <freidankm@yahoo.de>
2017-10-23 13:40:30 +0200

    interim

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+
 setup(name="VimForAnki",
       version="1.0",
       url="https://github.com/MFreidank/VimForAnki",
       description="Use vim to rapidly write textfiles immediately importable into anki(1).",
-      long_description=open("README.rst".read()),
+      long_description=long_description,
       author="Moritz Freidank",
       maintainer="Moritz Freidank",
       maintainer_email="freidankm@yahoo.de",
