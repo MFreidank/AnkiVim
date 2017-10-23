5cec01b9847e1e35cc313b09272221f40b0eb2ba
MFreidank <freidankm@yahoo.de>
2017-10-23 19:02:19 +0200

    interim

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ with open("README.rst", "r") as f:
 VERSION = "1.0"
 
 
-setup(name="VimForAnki",
+setup(name="AnkiVim",
       version=VERSION,
       url="https://github.com/MFreidank/VimForAnki",
       download_url="https://github.com/MFreidank/VimForAnki/tarball/{}".format(VERSION),
       description="Use vim to rapidly write textfiles immediately importable into anki(1).",
       long_description=long_description,
       author="Moritz Freidank",
+      author_email="freidankm@yahoo.de",
       maintainer="Moritz Freidank",
       maintainer_email="freidankm@yahoo.de",
       license="MIT",
